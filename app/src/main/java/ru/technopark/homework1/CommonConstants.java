fbbb008d9bc39a9505c1ac00bb514c102b65fa2c
Ilya Goltsov <ilya_goltsov@list.ru>
2019-10-15 17:02:18 +0300

    final version

--- app/src/main/java/ru/technopark/homework1/CommonConstants.java
+++ app/src/main/java/ru/technopark/homework1/CommonConstants.java
@@ -2,7 +2,7 @@ package ru.technopark.homework1;
 
 class CommonConstants {
     static final String LIST_STATE = "numberListState";
+    static final String LIST_SIZE = "listSize";
     static final String EXTRA_NUMBER = "extraNumber";
     static final String EXTRA_NUMBER_COLOR = "extraNumberColor";
-    static final String LIST_SIZE = "listSize";
 }
